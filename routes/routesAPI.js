d98109fc1129231d9e26aca46894bb934a424e4d
parumahajan24 <parumahajan24@gmail.com>
2022-12-13 13:59:01 -0500

    initial setup for payment route and handlebars ; updated css ; updated validation in route
    updated email & password validation in routes, added payment route, updated logout handlebar, created payment handlebars, added css for logout and updated for error

--- routes/routesAPI.js
+++ routes/routesAPI.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 let userData = require("../data/users");
 
+let validationForm = require("../validation");
+
 //root route - login/registration page
 router.route("/").get(async (req, res) => {
   if (req.session.email) {
@@ -11,6 +13,7 @@ router.route("/").get(async (req, res) => {
   }
   res.render("userLogin", {
     title: "Enter details to login",
+    // registrationTrue: "Registered succesfully!"
   });
 });
 
@@ -18,7 +21,8 @@ router
   .route("/register")
   .get(async (req, res) => {
     if (req.session.email) {
-      res.redirect("/welcome");
+      //res.redirect("/welcome");
+      res.status(200).redirect("/welcome");
       return;
     }
     res.render("userRegister", {
@@ -48,7 +52,6 @@ router
       !age ||
       !lincenceNumber
     ) {
-      // console.log("inside register post method...", usernameInput);
       res.status(400).render("userRegister", {
         title: "SignUp",
         error: "All fields must have valid input",
@@ -56,24 +59,36 @@ router
       return;
     }
     //email validation
-    if (typeof email !== "string") {
+    let emailFlag = validationForm.validateEmail(email);
+    if (!emailFlag || typeof email !== "string") {
       res.status(400).render("userRegister", {
         title: "SignUp",
-        error: "Invalid email.",
+        error:
+          "Invalid input for email. Please follow the format: example@example.com",
       });
       return;
     }
+
     //password validation
+    /* let passwordFlag = validationForm.passwordValidate(hashPassword)
+    if (passwordFlag === true) {
+      throw `Error: Invalid Input for Password. It contains spaces`;
+    }
+    validation.passwordValidate(password);
+    if (password.length < 8) {
+      throw `Error: Password must be at least 8 characters`;
+    } */
+
     if (typeof hashPassword !== "string" || hashPassword.trim().length < 8) {
       res.status(400).render("userRegister", {
         title: "SignUp",
-        error: "Enter a valid password",
+        error: "Enter a valid password with minimum length of 8",
       });
       return;
     }
 
     //firstName validation
-    if (typeof firstName !== "string" || firstName.trim().length < 0) {
+    if (typeof firstName !== "string" || firstName.trim().length === 0) {
       res.status(400).render("userRegister", {
         title: "SignUp",
         error: "Enter a valid first name",
@@ -82,7 +97,7 @@ router
     }
 
     //lastName validation
-    if (typeof lastName !== "string" || lastName.trim().length < 0) {
+    if (typeof lastName !== "string" || lastName.trim().length === 0) {
       res.status(400).render("userRegister", {
         title: "SignUp",
         error: "Enter a valid last name",
@@ -90,9 +105,14 @@ router
       return;
     }
 
-    // console.log("age..,", typeof age)
     //age validation
-    if (typeof age !== "string" || age < 1 || age > 100 || age == "") {
+    if (
+      typeof age !== "string" ||
+      age < 1 ||
+      age > 100 ||
+      age == "" ||
+      age % 1 != 0
+    ) {
       res.status(400).render("userRegister", {
         title: "SignUp",
         error: "Enter a valid age",
@@ -100,13 +120,16 @@ router
       return;
     }
 
+    //lincenceNumber validation
+    let licenseNumberFlag = validationForm.checkSpace(lincenceNumber);
     if (
+      licenseNumberFlag ||
       typeof lincenceNumber !== "string" ||
-      lincenceNumber.trim().length < 15
+      lincenceNumber.trim().length !== 15
     ) {
       res.status(400).render("userRegister", {
         title: "SignUp",
-        error: "Enter a valid lincence number",
+        error: "Enter a valid license number with no space",
       });
       return;
     }
@@ -147,14 +170,18 @@ router.route("/login").post(async (req, res) => {
     });
     return;
   }
-  //not sure if we need this validation for email
-  if (typeof email !== "string") {
-    res.status(400).render("userLogin", {
-      title: "Enter details to login",
-      error: "Invalid email.",
+  //email validation
+  let emailFlag = validationForm.validateEmail(email);
+  if (!emailFlag || typeof email !== "string") {
+    res.status(400).render("userRegister", {
+      title: "SignUp",
+      error:
+        "Invalid input for email. Please follow the format: example@example.com",
+      //registrationFalse:"Registration is unsuccessfull.",
     });
     return;
   }
+
   if (typeof hashPassword !== "string" || hashPassword.trim().length < 8) {
     res.status(400).render("userLogin", {
       title: "Enter details to login",
@@ -190,11 +217,27 @@ router.route("/welcome").get(async (req, res) => {
   });
 });
 
+//if booking successfull - route to payment
+router.route("/payment").get(async (req, res) => {
+  if (req.session.email) {
+    // might have to check if booking is done successfull or not
+    console.log("inside if .. emai -", req.session.email);
+    res.render("paymentPage", {
+      title: "Payment",
+    });
+    return;
+  }
+
+  res.render("userLogin", {
+    title: "Enter details to login",
+  });
+});
+
 router.route("/logout").get(async (req, res) => {
   //code here for GET
   req.session.destroy();
   res.render("logout", {
-    title: "Logout",
+    title: "Logged Out",
   });
 });
 
