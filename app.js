ee0211fbc29958e8afbe2399741a4af1e1f4a3a2
Mohak-Nagaraju <mnagaraj@stevens.edu>
2022-12-15 14:58:14 -0500

    changed in app.js and routes->index.js

--- app.js
+++ app.js
@@ -33,7 +33,23 @@ app.set('view engine', 'handlebars');
 //         next();
 //     }
 // })
-
+app.use('/protected', (req, res, next) => {
+  //console.log(req.session.id);
+  if (!req.session.user) {
+      res.status(403).render("forbiddenAccess", {title: "Not logged in" });
+  } else {
+      next();
+  }
+});
+app.use('/login', (req, res, next) => {
+  if (req.session.user) {
+    return res.redirect('/protected');
+  } else {
+    //here I',m just manually setting the req.method to post since it's usually coming from a form
+    req.method = 'POST';
+    next();
+  }
+});
 
 
 app.use((req,res,next) => {
