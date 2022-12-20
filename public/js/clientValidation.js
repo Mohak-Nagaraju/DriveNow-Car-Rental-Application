e6d584973f7aeffd45360088d607432f8171dcba
parumahajan24 <parumahajan24@gmail.com>
2022-12-19 22:44:51 -0500

    client validation updates

--- public/js/clientValidation.js
+++ public/js/clientValidation.js
@@ -95,9 +95,14 @@ function checkLincenceNumber(lincenceNumber, valName) {
 (function ($) {
   $(".login-error-div").hide();
   $(".signup-error-div").hide();
+  $(".booking-error-div").hide();
 
   const loginForm = $("#login-form");
   const registerForm = $("#signup-form");
+  const emailSendForm = $("#email-send-form");
+  const bookingForm = $("#booking-form");
+  const bookingFormInput = $("#booking-form-input");
+  const bookingFormCarRating = $("#booking-form-carRating");
 
   /*
     AJAX form to logout user 
@@ -110,13 +115,211 @@ function checkLincenceNumber(lincenceNumber, valName) {
       url: "/protected/logout",
     };
 
-    
     $.ajax(requestConfig).then(function (responseMessage) {
       //console.log('responseMessage',responseMessage)
       $("html").html(responseMessage);
     });
-    
+
     //req.session.destroy();
+  });
+
+  //booking Form car rating validation
+  $(bookingFormCarRating).submit(function (event) {
+    event.preventDefault();
+    var carRating = $("#carRating").val();
+    try {
+      carRating = checkString(carRating, "Car Rating");
+
+      if (parseFloat(carRating) === NaN) {
+        throw "Error: rating is not a number";
+      }
+
+      let splitRating = carRating.split("");
+
+      if (splitRating[1] === ".") {
+        if (splitRating.length !== 3) {
+          throw "Error: rating should be upto only one decimal place like 2.3";
+        }
+      }
+
+      if (splitRating[1] === ".") {
+        if (splitRating[0] < "1" || splitRating[0] > "5") {
+          throw "Error: rating should be between 1 and 5";
+        }
+      }
+
+      if (splitRating[1] === ".") {
+        if (splitRating[0] === "5") {
+          if (splitRating[2] !== "0") {
+            throw "Error: rating of 5 cannot have decimal values greater than or less than 0";
+          }
+        }
+      }
+
+      if (splitRating[1] === ".") {
+        if (splitRating[0] >= "1" || splitRating[0] <= "5") {
+          if (splitRating[2] >= 10) {
+            throw "Error: rating should be between 1.0 to 4.9";
+          }
+        }
+      }
+
+      rating = parseFloat(carRating);
+
+      if (rating < 1 || rating > 5) {
+        throw "Error: rating should be between 1 and 5";
+      }
+
+        $(this).unbind();
+        $(this).submit();
+     } catch (e) {
+      //console.log("error inside client validation..", e);
+      $(".booking-error-div").text(e);
+      $(".booking-error-div").show();
+      $("#booking-form-carRating").trigger("reset");
+    }
+  });
+
+  //UpdatBookingForm validation - with pickUpLocation
+  $(bookingFormInput).submit(function (event) {
+    event.preventDefault();
+    var pickUpDate = $("#pickUpDate").val();
+    var pickUpTime = $("#pickUpTime").val();
+    var returnDate = $("#returnDate").val();
+    var returnTime = $("#returnTime").val();
+    var pickUpLocation = $("#pickUpLocation").val();
+    try {
+      if (
+        !pickUpDate ||
+        !pickUpTime ||
+        !returnTime ||
+        !returnDate ||
+        !pickUpLocation
+      ) { 
+        throw "Error:Please enter all the values to book a car";
+      }
+
+      pickUpDate = checkString(pickUpDate, "Pick Up date");
+      pickUpTime = checkString(pickUpTime, "Pick Up Time");
+      returnTime = checkString(returnTime, "Return Time");
+      returnDate = checkString(returnDate, "Return date");
+      pickUpLocation = checkString(pickUpLocation, "Pickup Location");
+
+      split = pickUpDate.split("-");
+      const today = new Date();
+      const year = today.getFullYear();
+      let month = today.getMonth() + 1;
+      let day = today.getDate();
+      if (split[2] < day || split[1] < month || split[0] < year) {
+        throw "Error: Cannot select PickUp Date in the past";
+      }
+
+      split2 = returnDate.split("-");
+      if (
+        split2[2] < split[2] ||
+        split2[1] < split[1] ||
+        split2[0] < split[0]
+      ) {
+        throw `Error: Cannot select Return Date (${returnDate}) before the PickUp date (${pickUpDate})`;
+      }
+
+      if (pickUpDate === returnDate) {
+        let pickUpTimeSplit = pickUpTime.split(":");
+        let returnTimeSplit = returnTime.split(":");
+
+        if (pickUpTimeSplit[0] >= returnTimeSplit[0]) {
+          if (pickUpTimeSplit[1] >= returnTimeSplit[1]) {
+            throw `Error: Cannot select return time (${returnTime}) before or same as the pickup time (${pickUpTime})`;
+          }
+        }
+      }
+
+      if (pickUpLocation.length < 2 || pickUpLocation.length > 20) {
+        throw "Error: Invalid Input for Location.";
+      }
+
+      $(this).unbind();
+      $(this).submit();
+    } catch (e) {
+      //console.log("error inside client validation..", e);
+      $(".booking-error-div").text(e);
+      $(".booking-error-div").show();
+      $("#booking-form").trigger("reset");
+    }
+  });
+
+  //UpdatBookingForm validation
+  $(bookingForm).submit(function (event) {
+    event.preventDefault();
+    var pickUpDate = $("#pickUpDate").val();
+    var pickUpTime = $("#pickUpTime").val();
+    var returnDate = $("#returnDate").val();
+    var returnTime = $("#returnTime").val();
+    try {
+      if (!pickUpDate || !pickUpTime || !returnTime || !returnDate) {
+        throw "Please enter all the values to book a car";
+      }
+
+      pickUpDate = checkString(pickUpDate, "Pick Up date");
+      pickUpTime = checkString(pickUpTime, "Pick Up Time");
+      returnTime = checkString(returnTime, "Return Time");
+      returnDate = checkString(returnDate, "Return date");
+
+      split = pickUpDate.split("-");
+      const today = new Date();
+      const year = today.getFullYear();
+      let month = today.getMonth() + 1;
+      let day = today.getDate();
+      if (split[2] < day || split[1] < month || split[0] < year) {
+        throw "Error: Cannot select PickUp Date in the past";
+      }
+
+      split2 = returnDate.split("-");
+      if (
+        split2[2] < split[2] ||
+        split2[1] < split[1] ||
+        split2[0] < split[0]
+      ) {
+        throw `Error: Cannot select Return Date (${returnDate}) before the PickUp date (${pickUpDate})`;
+      }
+
+      if (pickUpDate === returnDate) {
+        let pickUpTimeSplit = pickUpTime.split(":");
+        let returnTimeSplit = returnTime.split(":");
+
+        if (pickUpTimeSplit[0] >= returnTimeSplit[0]) {
+          if (pickUpTimeSplit[1] >= returnTimeSplit[1]) {
+            throw `Error: Cannot select return time (${returnTime}) before or same as the pickup time (${pickUpTime})`;
+          }
+        }
+      }
+
+      $(this).unbind();
+      $(this).submit();
+    } catch (e) {
+      //console.log("error inside client validation..", e);
+      $(".booking-error-div").text(e);
+      $(".booking-error-div").show();
+      $("#booking-form").trigger("reset");
+    }
+  });
+
+  //sendEmail validation
+  $(emailSendForm).submit(function (event) {
+    event.preventDefault();
+    var email = $("#email").val();
+    try {
+      email = checkEmail(email);
+
+      $(this).unbind();
+      $(this).submit();
+    } catch (e) {
+      //console.log("error inside client validation..", e);
+      $(".email-error-div").text(e);
+      $(".email-error-div").show();
+      $("#email-form").trigger("reset");
+      $("#email").trigger("focus");
+    }
   });
 
   //login form validation
